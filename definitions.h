746a3d9fbf5006947a0d6e768d033ef6e877c900
Daniela Brazão <daniela.brazao.maksoud@gmail.com>
2015-11-11 23:33:53 -0200

    oi

--- definitions.h
+++ definitions.h
@@ -3,42 +3,37 @@
  * ~ ARQUIVO DE CABEÇALHO DE DEFINIÇÕES
  * -------------------------------------------------
  * 
- * Descrição: Arquivo de cabeçalho contendo definições de bibliotecas, macros e tipos.
- * Arquivo: definitions.h criado em 5 de Setembro de 2015, às 12:03.
+ *  Descrição: Arquivo de cabeçalho contendo definições de bibliotecas, macros e tipos.
+ *  Arquivo: definitions.h criado em 5 de Setembro de 2015, às 12:03.
+ *  Professor: Luiz Fernando Bessa Seibel
+ *  Instituição: Pontifícia Universidade Católica do Rio de Janeiro
+ *  Disciplina: Sistemas de Computação (INF1019)
+ *  Autoras: 
+ * - - - - - - - - - - - - - - - - - - - - - - - - -
+ *  -> Daniela Brazão Maksoud
+ *  -> Curso: Ciência da Computação
+ *  -> Matrícula: 1321873
+ * - - - - - - - - - - - - - - - - - - - - - - - - -
+ *  -> Thaíssa Falbo
+ *  -> Curso: Engenharia da Computação
+ *  -> Matrícula: 1421110
+ * - - - - - - - - - - - - - - - - - - - - - - - - -
  * 
- * Autoras: 
- * -------------------------------------------------
- * -> Daniela Brazão Maksoud
- * -> Curso: Ciência da Computação
- * -> Matrícula: 1321873
- * -------------------------------------------------
- * -> Thaíssa Falbo
- * -> Curso: Engenharia da Computação
- * -> Matrícula: 1421110
- * -------------------------------------------------
- * 
- * Professor: Luiz Fernando Bessa Seibel
- * Instituição: Pontifícia Universidade Católica do Rio de Janeiro
- * Disciplina: Sistemas de Computação (INF1019)
- * 
- * -------------------------------------------------
- * ~ COPYRIGHT
- * -------------------------------------------------
- * 
- * Este arquivo é parte do programa principal do Simulador de Gerenciador de Memória.
+ *  Este arquivo é parte do programa principal do Simulador de Gerenciador 
+ *  de Memória.
  *
- * O Simulador de Gerenciador de Memória é um software livre; você 
- * pode redistribuí-lo e/ou modificá-lo dentro dos termos da Licença 
- * Pública Geral GNU como publicada pela Fundação do Software Livre 
- * (FSF); na versão 3 da Licença.
+ *  O Simulador de Gerenciador de Memória é um software livre; você 
+ *  pode redistribuí-lo e/ou modificá-lo dentro dos termos da Licença 
+ *  Pública Geral GNU como publicada pela Fundação do Software Livre 
+ *  (FSF); na versão 3 da Licença.
  *
- * Este programa é distribuído na esperança de que possa ser útil, 
- * mas SEM NENHUMA GARANTIA; sem uma garantia implícita de ADEQUAÇÃO
- * a qualquer MERCADO ou APLICAÇÃO EM PARTICULAR. Veja a
- * Licença Pública Geral GNU para maiores detalhes.
+ *  Este programa é distribuído na esperança de que possa ser útil, 
+ *  mas SEM NENHUMA GARANTIA; sem uma garantia implícita de ADEQUAÇÃO
+ *  a qualquer MERCADO ou APLICAÇÃO EM PARTICULAR. Veja a
+ *  Licença Pública Geral GNU para maiores detalhes.
  *
- * Você deve ter recebido uma cópia da Licença Pública Geral GNU junto
- * com este programa, Se não, veja <http://www.gnu.org/licenses/>.
+ *  Você deve ter recebido uma cópia da Licença Pública Geral GNU junto
+ *  com este programa, Se não, veja <http://www.gnu.org/licenses/>.
  * 
  */
 
