6a8b86711d41f37db8ec9deea3aa210c4675fcae
Daniela Brazão <daniela.brazao.maksoud@gmail.com>
2015-11-11 23:37:42 -0200

    oi

--- main.c
+++ main.c
@@ -1,19 +1,40 @@
 /*
- * =*=*= PROGRAMA PRINCIPAL DO SIMULADOR DE GERENCIADOR DE MEMÓRIA =*=*=
- * Arquivo: main.c criado em 5 de Novembro de 2015, às 12:00.
- * Autoras:
- * ----------------------------------
- * -> Daniela Brazão Maksoud
- * -> Curso: Ciência da Computação
- * -> Matrícula: 1321873
- * ----------------------------------
- * -> Thaíssa Falbo
- * -> Curso: Engenharia da Computação
- * -> Matrícula: 1421110
- * ----------------------------------
- * Professor: Luiz Fernando Bessa Seibel
- * Instituição: Pontifícia Universidade Católica do Rio de Janeiro
- * Disciplina: Sistemas de Computação(INF1019)
+ * -----------------------------------------------------------------
+ * ~ PROGRAMA PRINCIPAL DO SIMULADOR DE GERENCIADOR DE MEMÓRIA
+ * -----------------------------------------------------------------
+ * 
+ *  Descrição: Arquivo de código fonte contendo o programa principal.
+ *  Arquivo: main.c criado em 5 de Novembro de 2015, às 12:00.
+ *  Professor: Luiz Fernando Bessa Seibel
+ *  Instituição: Pontifícia Universidade Católica do Rio de Janeiro
+ *  Disciplina: Sistemas de Computação (INF1019)
+ *  Autoras: 
+ * - - - - - - - - - - - - - - - - - - - - - - - - -
+ *  -> Daniela Brazão Maksoud
+ *  -> Curso: Ciência da Computação
+ *  -> Matrícula: 1321873
+ * - - - - - - - - - - - - - - - - - - - - - - - - -
+ *  -> Thaíssa Falbo
+ *  -> Curso: Engenharia da Computação
+ *  -> Matrícula: 1421110
+ * - - - - - - - - - - - - - - - - - - - - - - - - -
+ * 
+ *  Este arquivo é parte do programa principal do Simulador de Gerenciador 
+ *  de Memória.
+ *
+ *  O Simulador de Gerenciador de Memória é um software livre; você 
+ *  pode redistribuí-lo e/ou modificá-lo dentro dos termos da Licença 
+ *  Pública Geral GNU como publicada pela Fundação do Software Livre 
+ *  (FSF); na versão 3 da Licença.
+ *
+ *  Este programa é distribuído na esperança de que possa ser útil, 
+ *  mas SEM NENHUMA GARANTIA; sem uma garantia implícita de ADEQUAÇÃO
+ *  a qualquer MERCADO ou APLICAÇÃO EM PARTICULAR. Veja a
+ *  Licença Pública Geral GNU para maiores detalhes.
+ *
+ *  Você deve ter recebido uma cópia da Licença Pública Geral GNU junto
+ *  com este programa, Se não, veja <http://www.gnu.org/licenses/>.
+ * 
  */
 
 /* Pré-processamento */
