bc107094809f78b20c1d306a8e80a7a212150f3d
Daniela Brazão <daniela.brazao.maksoud@gmail.com>
2015-11-11 23:22:07 -0200

    oi

--- utilities.h
+++ utilities.h
@@ -27,18 +27,18 @@
  * 
  * Este arquivo é parte do programa principal do Simulador de Gerenciador de Memória.
  *
- * O Simulador de Gerenciador de Memória é um software livre; você 
- * pode redistribuí-lo e/ou modificá-lo dentro dos termos da Licença 
- * Pública Geral GNU como publicada pela Fundação do Software Livre 
- * (FSF); na versão 3 da Licença.
+ *  O Simulador de Gerenciador de Memória é um software livre; você 
+ *  pode redistribuí-lo e/ou modificá-lo dentro dos termos da Licença 
+ *  Pública Geral GNU como publicada pela Fundação do Software Livre 
+ *  (FSF); na versão 3 da Licença.
  *
- * Este programa é distribuído na esperança de que possa ser útil, 
- * mas SEM NENHUMA GARANTIA; sem uma garantia implícita de ADEQUAÇÃO
- * a qualquer MERCADO ou APLICAÇÃO EM PARTICULAR. Veja a
- * Licença Pública Geral GNU para maiores detalhes.
+ *  Este programa é distribuído na esperança de que possa ser útil, 
+ *  mas SEM NENHUMA GARANTIA; sem uma garantia implícita de ADEQUAÇÃO
+ *  a qualquer MERCADO ou APLICAÇÃO EM PARTICULAR. Veja a
+ *  Licença Pública Geral GNU para maiores detalhes.
  *
- * Você deve ter recebido uma cópia da Licença Pública Geral GNU junto
- * com este programa, Se não, veja <http://www.gnu.org/licenses/>.
+ *  Você deve ter recebido uma cópia da Licença Pública Geral GNU junto
+ *  com este programa, Se não, veja <http://www.gnu.org/licenses/>.
  * 
  */
 
